054ccfe0b9bae26d09682c994bd25747b5632294
Sreeginy <sreeginy98@gmail.com>
2023-07-16 10:52:52 +0530

    design changes

--- app/src/main/java/com/sreeginy/weather/WeatherHttpClient.java
+++ app/src/main/java/com/sreeginy/weather/WeatherHttpClient.java
@@ -18,7 +18,6 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
-import java.text.DecimalFormat;
 import java.text.ParseException;
 import java.util.ArrayList;
 import java.util.Date;
